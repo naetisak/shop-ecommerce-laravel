59e33070e24c18cfbc99444eb4d758d2be65239a
unknown <65301282014@utc.ac.th>
2024-02-27 07:07:57 +0700

    edit-new

--- resources/views/product_detail.blade.php
+++ resources/views/product_detail.blade.php
@@ -82,139 +82,122 @@
 @endpush
 
 @section('body_content')
-    <section class="px-6 md:px-20 mt-6">
+<section class="px-6 md:px-20 mt-6">
+    <div class="flex flex-wrap md:flex-nowrap gap-6 justify-center"> <!-- เพิ่มคลาส justify-center เพื่อจัดให้เนื้อหาอยู่ตรงกลาง -->
 
-        <div class="flex flex-wrap md:flex-nowrap gap-6">
-            {{-- Left --}}
-            <div class="shrink-0 w-full md:w-auto flex flex-col-reverse md:flex-row gap-4">
-                <div id="images" class="flex md:flex-col gap-3 pb-1 md:pb-0 max-h-96 overflow-y-auto">
-                    @foreach ($product->image as $image)
-                        <div onclick="viewImage(this, {{ $image->id }})"
-                            class="bg-white rounded-md shadow p-1 cursor-pointer">
-                            <img class="w-14" src="{{ asset('storage/' . $image->path) }}" alt="" />
-                        </div>
-                    @endforeach
-                </div>
-                <div class="h-96 relative bg-white rounded-md shadow-md p-3">
-                    <img id="bigImage" class="h-full aspect-[2/3]" src="{{ asset('storage/' . $product->image[0]->path) }}"
-                        alt="">
-
-                    <span onclick="nextPrevious(-1)"
-                        class="absolute top-1/2 left-1 bg-white rounded-full w-5 h-5 shadow flex items-center justify-center">
-                        <i
-                            class='bx bx-chevron-left text-xl text-gray-400 hover:text-violet-600 duration-200 cursor-pointer'></i>
-                    </span>
-                    <span onclick="nextPrevious(1)"
-                        class="absolute top-1/2 right-1 bg-white rounded-full w-5 h-5 shadow flex items-center justify-center">
-                        <i
-                            class='bx bx-chevron-right text-xl text-gray-400 hover:text-violet-600 duration-200 cursor-pointer'></i>
-                    </span>
-                </div>
-            </div>
-            {{-- Left End --}}
-
-            {{-- Right --}}
-            <div class="w-full flex flex-col gap-4">
-                <div class="flex gap-3">
-                    @php
-                        $discount = (($product->variant[0]->mrp - $product->variant[0]->selling_price) / $product->variant[0]->mrp) * 100;
-                    @endphp
-                    <span class="bg-red-500 text-white rounded px-2 text-xs">{{ round($discount, 2) }}% Off</span>
-                    <span class="text-gray-400 text-sm"><i class='bx bx-star'></i> 4.5</span>
-                </div>
-
-                {{-- Name, SKU, Brand --}}
-                <h2 class="text-lg font-medium text-gray-800">{{ $product->title }}</h2>
-                <div class="text-sm text-gray-800">
-                    <p><span class="text-gray-400">SKU:</span> {{ $product->variant[0]->sku }}</p>
-                    <p><span class="text-gray-400">Brand:</span> {{ $product->brand->name }}</p>
-                </div>
-
-                {{-- Price --}}
-                <div>
-                    <span class="text-rose-500 font-bold text-xl">₹{{ $product->variant[0]->selling_price }}</span>
-                    <sub class="text-gray-400"><strike>₹{{ $product->variant[0]->mrp }}</strike></sub>
-                </div>
-
-                {{-- Colors --}}
-                <div>
-                    <p class="text-gray-400">Colors:</p>
-                    <div class="flex gap-1">
-                        @foreach ($product->variant as $item)
-                            <span onclick="selectColor('{{ $item->color->id }}')"
-                                style="background-color: {{ $item->color->code }}"
-                                class="w-5 h-5 cursor-pointer rounded-full">&nbsp;</span>
-                        @endforeach
+        {{-- Left --}}
+        <div class="shrink-0 w-full md:w-auto flex flex-col-reverse md:flex-row gap-4">
+            <div id="images" class="flex md:flex-col gap-2 pb-1 md:pb-0 max-h-96 overflow-y-auto">
+                @foreach ($product->image as $image)
+                    <div onclick="viewImage(this, {{ $image->id }})"
+                        class="bg-white rounded-md shadow p-1 cursor-pointer w-14 h-14 md:w-12 md:h-12 lg:w-16 lg:h-16">
+                        <img class="w-full h-full object-cover" src="{{ asset('storage/' . $image->path) }}" alt="" />
                     </div>
-                </div>
-
-                {{-- Sizes --}}
-                <div>
-                    <p class="text-gray-400">Sizes:</p>
-                    <div class="flex gap-1 text-gray-400 text-sm">
-                        @foreach ($product->variant as $item)
-                            <span onclick="selectSize('{{ $item->size->id }}')"
-                                class="flex justify-center cursor-pointer items-center w-5 h-5 rounded-full border border-gray-400">{{ $item->size->code }}</span>
-                        @endforeach
-                    </div>
-                    <a href="#" class="text-gray-400 text-xs">Size Guide</a>
-                </div>
-
-                {{-- Quantity --}}
-                {{-- <div>
-                    <p class="text-gray-400">Quantity</p>
-                    <div class="flex items-center gap-2">
-                        <input type="text" value="1" readonly
-                            class="bg-slate-200 rounded border border-gray-300 focus:outline-none px-2 text-lg font-medium w-20">
-                        <button class="rounded border border-gray-300 w-7 h-7"><i class='bx bx-minus text-xl'></i></button>
-                        <button class="rounded border border-gray-300 w-7 h-7"><i class='bx bx-plus text-xl'></i></button>
-                    </div>
-                </div> --}}
-
-                {{-- Wishlist, Add to Cart and Buy Now --}}
-                <div class="flex items-center gap-4">
-                    <button onclick="toggleWishlist(this, '{{ $product->id }}')" class="bg-white shadow-md rounded-full w-7 h-7 flex items-center justify-center">
-                        <i class='bx  {{ $product->has_favorited ? 'bxs-heart text-red-500' : 'bx-heart' }} text-xl'></i>
-                    </button>
-
-                    <button onclick="addToCart()" id="add_to_cart_btn"
-                        class="border border-violet-600 rounded w-28 text-center drop-shadow font-medium text-violet-600 py-0.5">Add
-                        to Cart</button>
-                    <button onclick="buyNow()"
-                        class="border border-violet-600 rounded w-28 text-center drop-shadow font-medium text-white bg-violet-600 py-0.5">Buy
-                        Now</button>
-                </div>
-
-            </div>
-            {{-- Right End  --}}
-        </div>
-
-        {{-- Product Description --}}
-        <div>
-            <h3 class="text-lg text-gray-400 font-medium my-6">Product Description</h3>
-            <div class="text-gray-600">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad tenetur consectetur aspernatur veritatis rem,
-                asperiores necessitatibus corporis doloremque repellendus et cumque reprehenderit, nisi vitae autem iste
-                quia temporibus, fugiat ea voluptates velit tempora quae! Cupiditate debitis eaque ex. Similique aperiam
-                rerum inventore est fugiat, optio amet ratione sequi doloribus illum iure suscipit voluptatum ut repellat
-                non, impedit, et harum? Ipsam porro deleniti voluptatum fugiat quibusdam. Blanditiis eos perspiciatis
-                voluptatum. Deserunt praesentium fuga quisquam neque possimus, adipisci officiis. Sit quo a voluptas quidem
-                minima debitis culpa aliquam voluptatibus repellendus repudiandae explicabo totam, quaerat odit, in alias
-                facere ullam, commodi expedita fugit!
-            </div>
-        </div>
-
-
-        <section class="mt-6">
-            <h3 class="text-gray-800 font-medium mb-2">Featured Product</h3>
-            <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
-                @foreach ($products as $item)
-                    @if ($item->variant->isNotEmpty())
-                        <x-product.card1 :product="$item" />
-                    @endif
                 @endforeach
             </div>
-        </section>
+            <div class="h-auto md:h-96 relative bg-white rounded-md shadow-md p-3">
+                <img id="bigImage" class="w-full h-full object-contain" src="{{ asset('storage/' . $product->image[0]->path) }}" alt="">
+                <span onclick="nextPrevious(-1)" class="absolute top-1/2 left-1 bg-white rounded-full w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 shadow flex items-center justify-center">
+                    <i class='bx bx-chevron-left text-xl text-gray-400 hover:text-violet-600 duration-200 cursor-pointer'></i>
+                </span>
+                <span onclick="nextPrevious(1)" class="absolute top-1/2 right-1 bg-white rounded-full w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 shadow flex items-center justify-center">
+                    <i class='bx bx-chevron-right text-xl text-gray-400 hover:text-violet-600 duration-200 cursor-pointer'></i>
+                </span>
+            </div>
+        </div>
+        {{-- Left End --}}
 
+        {{-- Right --}}
+        <div class="w-full md:w-1/3 flex flex-col gap-4 bg-white p-6 rounded-md shadow-md">
+            <div class="flex gap-3">
+                @php
+                    $discount = (($product->variant[0]->mrp - $product->variant[0]->selling_price) / $product->variant[0]->mrp) * 100;
+                @endphp
+                <span class="bg-red-500 text-white rounded px-2 text-xs">{{ round($discount, 2) }}% Off</span>
+                <span class="text-gray-400 text-sm"><i class='bx bx-star'></i> 4.5</span>
+            </div>
+
+            {{-- Name, SKU, Brand --}}
+            <h2 class="text-lg font-medium text-gray-800">{{ $product->title }}</h2>
+            <div class="text-sm text-gray-800">
+                <p><span class="text-gray-400">SKU:</span> {{ $product->variant[0]->sku }}</p>
+                <p><span class="text-gray-400">Brand:</span> {{ $product->brand->name }}</p>
+            </div>
+
+            {{-- Price --}}
+            <div>
+                <span class="text-rose-500 font-bold text-xl">₹{{ $product->variant[0]->selling_price }}</span>
+                <sub class="text-gray-400"><strike>₹{{ $product->variant[0]->mrp }}</strike></sub>
+            </div>
+
+            {{-- Colors --}}
+            <div class="mt-4"> <!-- เพิ่มคลาส mt-4 เพื่อเพิ่มระยะห่างด้านบน -->
+                <p class="text-gray-400">Colors:</p>
+                <div class="flex gap-1">
+                    @foreach ($product->variant as $item)
+                        <span onclick="selectColor('{{ $item->color->id }}')"
+                            style="background-color: {{ $item->color->code }}"
+                            class="w-5 h-5 cursor-pointer rounded-full">&nbsp;</span>
+                    @endforeach
+                </div>
+            </div>
+
+            {{-- Sizes --}}
+            <div>
+                <p class="text-gray-400">Sizes:</p>
+                <div class="flex gap-1 text-gray-400 text-sm">
+                    @foreach ($product->variant as $item)
+                        <span onclick="selectSize('{{ $item->size->id }}')"
+                            class="flex justify-center cursor-pointer items-center rounded-full border border-gray-400 px-2 py-1">{{ $item->size->code }}</span>
+                    @endforeach
+                </div>
+                <a href="#" class="text-gray-400 text-xs">Size Guide</a>
+            </div>
+
+
+            {{-- Wishlist, Add to Cart and Buy Now --}}
+            <div class="flex items-center gap-4 mt-4"> <!-- เพิ่มคลาส mt-4 เพื่อเพิ่มระยะห่างด้านบน -->
+                <button onclick="toggleWishlist(this, '{{ $product->id }}')" class="bg-white shadow-md rounded-full w-7 h-7 flex items-center justify-center">
+                    <i class='bx  {{ $product->has_favorited ? 'bxs-heart text-red-500' : 'bx-heart' }} text-xl'></i>
+                </button>
+
+                <button onclick="addToCart()" id="add_to_cart_btn"
+                    class="border border-violet-600 rounded w-28 text-center drop-shadow font-medium text-violet-600 py-0.5">Add
+                    to Cart</button>
+                <button onclick="buyNow()"
+                    class="border border-violet-600 rounded w-28 text-center drop-shadow font-medium text-white bg-violet-600 py-0.5">Buy
+                    Now</button>
+            </div>
+
+        </div>
+        {{-- Right End --}}
+    </div>
+
+    {{-- Product Description --}}
+    <div class="mt-6">
+        <h3 class="text-lg text-gray-400 font-medium mb-2">Product Description</h3>
+        <div class="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad tenetur consectetur aspernatur veritatis rem,
+            asperiores necessitatibus corporis doloremque repellendus et cumque reprehenderit, nisi vitae autem iste
+            quia temporibus, fugiat ea voluptates velit tempora quae! Cupiditate debitis eaque ex. Similique aperiam
+            rerum inventore est fugiat, optio amet ratione sequi doloribus illum iure suscipit voluptatum ut repellat
+            non, impedit, et harum? Ipsam porro deleniti voluptatum fugiat quibusdam. Blanditiis eos perspiciatis
+            voluptatum. Deserunt praesentium fuga quisquam neque possimus, adipisci officiis. Sit quo a voluptas quidem
+            minima debitis culpa aliquam voluptatibus repellendus repudiandae explicabo totam, quaerat odit, in alias
+            facere ullam, commodi expedita fugit!
+        </div>
+    </div>
+
+    <section class="mt-6">
+        <h3 class="text-gray-800 font-medium mb-2">Featured Product</h3>
+        <div class="grid grid-cols-1 md:grid-cols-4 gap-6 justify-center"> <!-- เพิ่มคลาส justify-center เพื่อจัดให้เนื้อหาอยู่ตรงกลาง -->
+            @foreach ($products as $item)
+                @if ($item->variant->isNotEmpty())
+                    <x-product.card1 :product="$item" />
+                @endif
+            @endforeach
+        </div>
     </section>
+
+</section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,58 +1,49 @@
 <?php
 
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Artisan;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\WishlistController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', [HomeController::class, 'index'])->name('landing-page');
+Route::get('/pd/{slug}', [HomeController::class, 'productDetail'])->name('product_detail');
+Route::get('/products', [HomeController::class, 'products'])->name('products');
 
-Route::controller(\App\Http\Controllers\HomeController::class)->group(function () {
-    Route::get('/', 'index')->name('landing-page');
-    Route::get('/pd/{slug}', 'productDetail')->name('product_detail');
-    Route::get('/products', 'products')->name('products');
+// Auth Routes
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/forgot', [AuthController::class, 'forgot'])->name('forgot');
+Route::match(['GET', 'POST'], '/update-password', [AuthController::class, 'updatePassword'])->name('update-password');
+
+// Account Routes
+Route::prefix('account')->group(function () {
+    Route::get('/', [AccountController::class, 'index'])->name('account.index');
+    Route::post('/', [AccountController::class, 'index'])->name('account.index');
+    Route::get('orders/{id}', [AccountController::class, 'showOrder'])->name('order.show');
+    Route::get('address', [AccountController::class, 'newAddress'])->name('address.create');
+    Route::post('address', [AccountController::class, 'newAddress'])->name('address.store');
+    Route::get('address/{id}', [AccountController::class, 'editAddress'])->name('address.edit');
+    Route::put('address/{id}', [AccountController::class, 'editAddress'])->name('address.update');
 });
 
-Route::controller(App\Http\Controllers\AuthController::class)->group(function () {
-    Route::get('/logout', 'logout')->name('logout');
-    Route::post('/login', 'login')->name('login');
-    Route::post('/register', 'register')->name('register');
-    Route::post('/forgot', 'forgot')->name('forgot');
-    Route::match(['GET', 'POST'], '/update-password', 'updatePassword')->name('update-password');
-});
+// Cart Routes
+Route::get('/cart', [CartController::class, 'index'])->name('cart');
+Route::get('/cart/products', [CartController::class, 'apiCartProducts']);
+Route::post('/cart/coupon', [CartController::class, 'apiApplyCoupon']);
+Route::post('/payment/init', [CartController::class, 'initPayment'])->name('payment.init');
+Route::post('/payment/failed', [CartController::class, 'paymentFailed'])->name('payment.fail');
+Route::post('/payment/verify/{id}', [CartController::class, 'paymentVerify']);
 
-Route::controller(App\Http\Controllers\AccountController::class)->group(function () {
+// Wishlist Routes
+Route::get('/wishlist', [WishlistController::class, 'index'])->name('wishlist');
+Route::post('/wishlist/{id}', [WishlistController::class, 'toggle']);
 
-    Route::prefix('account')->group(function () {
-        Route::get('orders/{id}', 'showOrder')->name('order.show');
-        Route::get('address', 'newAddress')->name('address.create');
-        Route::post('address', 'newAddress')->name('address.store');
-        Route::get('address/{id}', 'editAddress')->name('address.edit');
-        Route::put('address/{id}', 'editAddress')->name('address.update');
-    });
+// Payment Route
+Route::post('/create-payment-intent', [PaymentController::class, 'createPaymentIntent']);
 
-    Route::get('account/', 'index')->name('account.index');
-    Route::post('account/', 'index')->name('account.index');
-});
 
-Route::controller(\App\Http\Controllers\CartController::class)->group(function () {
-    Route::get('/cart', 'index')->name('cart');
-    Route::get('/cart/products', 'apiCartProducts');
-    Route::post('/cart/coupon', 'apiApplyCoupon');
-    Route::post('/payment/init', 'initPayment')->name('payment.init');
-    Route::post('/payment/failed', 'paymentFailed')->name('payment.fail');
-    Route::post('/payment/verify/{id}', 'paymentVerify');
-});
-
-Route::controller(\App\Http\Controllers\WishlistController::class)->group(function () {
-    Route::get('/wishlist', 'index')->name('wishlist');
-    Route::post('/wishlist/{id}', 'toggle');
-});
